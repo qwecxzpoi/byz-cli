f81675141fbf3d0946a4d5cfcac8df63dd7fad1e
qwecxzpoi <benyuanzhanggithub@outlook.com>
2023-11-06 01:41:43 +0800

    chore: 移除 unplugin-auto-import

--- template-vue-ts/vite.config.ts
+++ template-vue-ts/vite.config.ts
@@ -3,21 +3,12 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
-import AutoImport from 'unplugin-auto-import/vite'
 import { presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      dts: true,
-      imports: [
-        'vue',
-        'vue-router',
-        'pinia',
-      ],
-    }),
     Unocss({
       presets: [
         presetUno(),
